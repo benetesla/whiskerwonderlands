d0de09eac888fe7b819c30485c7688f980bc42e1
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-17 13:28:45 -0300

    Atualizando o readme do sistema

---
+++ src/Components/CatApi.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable react/style-prop-object */
+import React, { useState, useEffect } from 'react'
+import './CatApi.css'
+
+
+const CatApi = () => {
+    const ApiUrl = 'https://catfact.ninja/fact'
+    const [cat, setCat] = useState([])
+    const [loading, setLoading] = useState(true)
+    const randomFact = () => {
+        const fetctData = async () => {
+            const response = await fetch(ApiUrl)
+            const data = await response.json()
+            setCat(data)
+            setLoading(false)
+        }
+        fetctData()
+
+    }
+
+    useEffect(() => {
+        const fetctData = async () => {
+            const response = await fetch(ApiUrl)
+            const data = await response.json()
+            setCat(data)
+            setLoading(false)
+        }
+        fetctData()
+    }, [])
+    if (loading) {
+        return <h2>
+            <div class="spinner">
+                <div class="spinner1"></div>
+            </div>
+        </h2>
+    }
+    return (
+        <div className="card">
+            <div className="cat-card">
+                <button className="cat-button" onClick={randomFact}>Random Fact</button>
+                <h1 className="cat-title">Cat Fact</h1>
+                <p className="cat-text">{cat.fact}</p>
+                <p className="cat-text">- {cat.length}</p>
+            </div>
+        </div>
+    )
+}
+
+export default CatApi
